75cfb23b2c3030a92661348c35e0b457c49e0674
Patrick Baumgart <baumgart.patrick@gmail.com>
2022-04-24 18:56:17 +0200

    Mini-Sandbox for tests around basics.

--- Preps_Sandbox/00__Sandbox/00__Sandbox.ino
+++ Preps_Sandbox/00__Sandbox/00__Sandbox.ino
@@ -1,6 +1,13 @@
-
 void setup() {
+  //Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  Serial.println("Alive!");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
